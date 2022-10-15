0c495c7c32f75270e340af1454842dc8c0302e53
JenniBeadle <>
2022-10-16 02:36:59 +0800

    Delete dsn.bat

--- dsn.bat
+++
@@ -1,7 +0,0 @@
-echo off
-rem run docu-notion for paratext-notion
-rem Jenni Beadle Oct 2022
-rem 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
-
-echo on
-npx docu-notion -n secret_c5SkXtz5nnsLJW0WR4Zt3HdVjz5o0teNG9EJnERVjRU -r c285465407be4e4fb42c8c6db7bf0cbe
